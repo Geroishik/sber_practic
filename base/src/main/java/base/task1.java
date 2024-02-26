2147cd4186a42b1187d9ba93a41fb5f31fde2599
Geroishik <ilya-manoylov@mail.ru>
2024-02-26 19:23:06 +0300

    Готовая первая задача

---
+++ base/src/main/java/base/task1.java
@@ -0,0 +1,9 @@
+package base;
+
+public class task1 {
+    public static void main(String[] args) {
+        for(int sevenMult = 7; sevenMult <= 98; sevenMult += 7 ){
+            System.out.printf("%d ", sevenMult);
+        }
+    }
+}
