44c90e9de33fa7b9e1d1a19e3fbba2e6c9bdd966
Geroishik <ilya-manoylov@mail.ru>
2024-05-02 10:52:12 +0300

    Готовая первая задача

--- base/src/main/java/OOP/Circle/CheckCircle.java
+++
@@ -1,8 +0,0 @@
-package OOP.Circle;
-
-public class CheckCircle {
-    public static void main(String[] args) {
-        Circle circle = new Circle(10, "red");
-        System.out.println(circle);
-    }
-}
